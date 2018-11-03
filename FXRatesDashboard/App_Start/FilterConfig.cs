30158efeab33e5b17932a2b43f95568e34324e1e
Conor McLafferty <mclaff@hotmail.com>
2018-11-03 20:50:49 +0000

    Add project files.

---
+++ FXRatesDashboard/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FXRatesDashboard
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
